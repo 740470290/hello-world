5ee5bdf85d7ed58bf6a2b10db327bfdf4675d2ed
740470290 <47243229+740470290@users.noreply.github.com>
2019-03-18 19:18:01 +0800

    Update app.py

--- flaskSqlite/app.py
+++ flaskSqlite/app.py
@@ -14,45 +14,40 @@ def index():
         # date = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())
         cur.execute("INSERT INTO students (name,password) VALUES(?, ?)",(user,text) )
         con.commit()
-        return redirect(url_for('index'))
-    else:
-        cur.execute("select * from students")
-        ulist = cur.fetchall()
-        return render_template('index.html', says=ulist)
+    cur.execute("select * from students")
+    ulist = cur.fetchall()
+    return render_template('index.html', says=ulist)
 @app.route('/del/', methods=['GET', 'POST'])
 def del_user():
     con = sql.connect("mydb")
     cur = con.cursor()
     # 根据摸个字段作删除,filter_by蕾仕于where条件限定
     # 翻译为：delete from user where id= 1
-    if request.method == 'GET':
-        cur.execute("select * from students")
-        ulist = cur.fetchall()
-        return render_template('index.html', says=ulist)
+    if request.method == 'POST':
+        userId = request.form.get('userId')
+        cur.execute("delete from students where id=" + userId)
+        cur.execute("update students set id=id-1 where id>" + userId)
+        con.commit()
     # else:
-    userId = request.form.get('userId')
-    cur.execute("delete from students where id="+userId)
-    cur.execute("update students set id=id-1 where id>"+userId)
-    con.commit()
-    return redirect(url_for('del_user'))
+    cur.execute("select * from students")
+    ulist = cur.fetchall()
+    return render_template('index.html', says=ulist)
 @app.route('/edits/', methods=['GET', 'POST'])
 def edit_user():
     con = sql.connect("mydb")
     cur = con.cursor()
     # 根据摸个字段作删除,filter_by蕾仕于where条件限定
     # 翻译为：delete from user where id= 1
-    if request.method == 'GET':
-        cur.execute("select * from students")
-        ulist = cur.fetchall()
-        return render_template('index.html', says=ulist)
+    if request.method == 'POST':
+        userId = request.form.get('userId')
+        user = request.form.get('say_user')
+        text = request.form.get('say_password')
+        cur.execute("update students set name=" + user + ", password =" + text + " where id=" + userId)
+        con.commit()
     # else:
-    userId = request.form.get('userId')
-    user = request.form.get('say_user')
-    text = request.form.get('say_password')
-    cur.execute("update students set name="+user+", password ="+text+" where id="+userId)
-    # cur.filter_by(id=userId).update({'name': user,'password': text})
-    con.commit()
-    return redirect(url_for('edit_user'))
+    cur.execute("select * from students")
+    ulist = cur.fetchall()
+    return render_template('index.html', says=ulist)
 
 # 程序入口
 if __name__ == '__main__':
