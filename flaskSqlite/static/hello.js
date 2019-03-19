029306cdc4ac66093ad51fe9caac1800ec171f01
740470290 <47243229+740470290@users.noreply.github.com>
2019-03-19 16:56:43 +0800

    Create hello.js

---
+++ flaskSqlite/static/hello.js
@@ -0,0 +1,52 @@
+function checkId() {
+    var x = document.forms["del"]["userId"].value;
+    var b = document.getElementsByTagName("tr").length;
+    if (isNaN(x) || x < 1 || x > b-1) {
+        alert("id输入错误");
+        return false;
+    }
+}
+function checkIdBelow() {
+    var c = document.forms["update"]["userId"].value;
+    var b = document.getElementsByTagName("tr").length;
+    var x = document.forms["sub"]["say_password"].value;
+    var a = document.forms["sub"]["say_user"].value;
+    var str="qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM";
+    var pas="_1234567890qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM";
+    var i,j;
+    if (isNaN(c) || c < 1 || c > b-1) {
+        alert("id输入错误");
+        return false;
+    }
+    for(i=0;i<x.length;i++){
+        if(pas.indexOf(x[i])==-1){
+        alert("不合法的密码");
+        return false;
+            }
+    }
+    for(j=0;j<a.length;j++){
+        if(str.indexOf(a[j])==-1){
+        alert("不合法的用户名");
+        return false;
+            }
+    }
+}
+function checkPassword() {
+    var x = document.forms["sub"]["say_password"].value;
+    var a = document.forms["sub"]["say_user"].value;
+    var str="qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM";
+    var pas="_1234567890qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM";
+    var i,j;
+    for(i=0;i<x.length;i++){
+        if(pas.indexOf(x[i])==-1){
+        alert("不合法的密码");
+        return false;
+            }
+    }
+    for(j=0;j<a.length;j++){
+        if(str.indexOf(a[j])==-1){
+        alert("不合法的用户名");
+        return false;
+            }
+    }
+}
