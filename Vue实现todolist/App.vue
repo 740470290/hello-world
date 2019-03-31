f61930a3f82c73d191f667ba8bbb58762e51fe5b
740470290 <47243229+740470290@users.noreply.github.com>
2019-03-31 22:15:47 +0800

    Add files via upload

---
+++ Vue实现todolist/App.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+    <div>
+      <form action="">
+        <input v-model="inputValue" type="text">
+        <button @click="handleSubmit" type="submit">提交</button>
+      </form>
+    </div>
+    <ul>
+      <todo-item v-for="(item, index) of list" :content="item"
+      :key="index" :index="index" @delete="handleDelete"
+      ></todo-item>
+    </ul>
+  </div>
+</template>
+<script>
+import Hello from './components/Hello'
+export default {
+  components: {
+    'todo-item': Hello
+  },
+  data () {
+    return {
+      inputValue: '',
+      list: []
+    }
+  },
+  methods: {
+    handleSubmit () {
+      this.list.push(this.inputValue)
+      this.inputValue = ''
+    },
+    handleDelete (index) {
+      this.list.splice(index, 1)
+    }
+  }
+}
+
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+</style>
---
+++ Vue实现todolist/Hello.vue
@@ -0,0 +1,18 @@
+<template>
+  <li @click="handleDelete">{{content}}</li>
+</template>
+
+<script>
+export default {
+  props: ['content', 'index'],
+  methods: {
+    handleDelete () {
+      this.$emit('delete', this.index)
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
