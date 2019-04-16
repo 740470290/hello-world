0399402f0fa63bcf7773e94087a72800f3a008b8
740470290 <47243229+740470290@users.noreply.github.com>
2019-04-16 22:26:39 +0800

    Add files via upload

---
+++ 网络爬虫/spider.py
@@ -0,0 +1,105 @@
+import pymongo
+from selenium import webdriver
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
+from pyquery import PyQuery as pq
+from 动态网页.config import *
+from urllib.parse import quote
+import json
+
+browser = webdriver.Chrome()
+# browser = webdriver.PhantomJS(service_args=SERVICE_ARGS)
+
+# chrome_options = webdriver.ChromeOptions()
+# chrome_options.add_argument('--headless')
+# browser = webdriver.Chrome(chrome_options=chrome_options)
+
+wait = WebDriverWait(browser, 10)
+client = pymongo.MongoClient(MONGO_URL)
+db = client[MONGO_DB]
+
+
+def index_page(page):
+    """
+    抓取索引页
+    :param page: 页码
+    """
+    print('正在爬取第', page, '页')
+    try:
+        # url = 'https://s.taobao.com/search?q=' + quote(KEYWORD)
+        url = 'https://s.taobao.com/search?q=ipad'
+        browser.get(url)
+        # head = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36',
+        #     'authority':'s.taobao.com',
+        #     'cookie':'',
+        #     'referer':'https://www.taobao.com/',
+        #     }
+        browser.delete_all_cookies()
+        f1 = open('cookie.txt')
+        cookie = f1.read()
+        cookie =json.loads(cookie)
+        for c in cookie:
+            browser.add_cookie(c)
+        # print(browser.get_cookies())
+        browser.get(url)
+        if page > 1:
+            input = wait.until(
+                EC.presence_of_element_located((By.CSS_SELECTOR, '#mainsrp-pager div.form > input')))
+            submit = wait.until(
+                EC.element_to_be_clickable((By.CSS_SELECTOR, '#mainsrp-pager div.form > span.btn.J_Submit')))
+            input.clear()
+            input.send_keys(page)
+            submit.click()
+        wait.until(
+            EC.text_to_be_present_in_element((By.CSS_SELECTOR, '#mainsrp-pager li.item.active > span'), str(page)))
+        wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, '.m-itemlist .items .item')))
+        get_products()
+    except TimeoutException:
+        index_page(page)
+
+
+def get_products():
+    """
+    提取商品数据
+    """
+    html = browser.page_source
+    doc = pq(html)
+    items = doc('#mainsrp-itemlist .items .item').items()
+    for item in items:
+        product = {
+            'image': item.find('.pic .img').attr('data-src'),
+            'price': item.find('.price').text(),
+            'deal': item.find('.deal-cnt').text(),
+            'title': item.find('.title').text(),
+            'shop': item.find('.shop').text(),
+            'location': item.find('.location').text()
+        }
+        print(product)
+        # save_to_mongo(product)
+
+
+def save_to_mongo(result):
+    """
+    保存至MongoDB
+    :param result: 结果
+    """
+    try:
+        if db[MONGO_COLLECTION].insert(result):
+            print('存储到MongoDB成功')
+    except Exception:
+        print('存储到MongoDB失败')
+
+
+def main():
+    """
+    遍历每一页
+    """
+    for i in range(1, MAX_PAGE + 1):
+        index_page(i)
+    browser.close()
+
+
+if __name__ == '__main__':
+    main()
