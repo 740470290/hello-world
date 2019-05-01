14cae13e3b3194bc3e8aaac346f3353a4b2d8996
740470290 <47243229+740470290@users.noreply.github.com>
2019-05-02 00:23:25 +0800

    增加了post方法

--- node文件/express.js
+++ node文件/express.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
  
+// 创建 application/x-www-form-urlencoded 编码解析
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
 app.use('/public', express.static('public'));
  
 app.get('/index', function (req, res) {
@@ -17,7 +20,16 @@ app.get('/process_get', function (req, res) {
    console.log(response);
    res.end(JSON.stringify(response));
 })
+app.post('/process_post', urlencodedParser, function (req, res) {
  
+   // 输出 JSON 格式
+   var response = {
+       "first_name":req.body.first_name,
+       "last_name":req.body.last_name
+   };
+   console.log(response);
+   res.end(JSON.stringify(response));
+}) 
 var server = app.listen(8080, function () {
  
   var host = server.address().address
