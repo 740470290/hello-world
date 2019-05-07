e18c41e6cae59b56371e631f570239470dae2e31
740470290 <47243229+740470290@users.noreply.github.com>
2019-05-07 10:00:26 +0800

    Update 01.php

--- Php文件/瀑布流php版/01.php
+++ Php文件/瀑布流php版/01.php
@@ -1,9 +1,11 @@
 <?php
 header('Content-Type: text/html; charset=utf-8');
 $json=file_get_contents('./info/data.json');
+echo $json;
 //echo json_decode($json);
-$parr=json_decode($json);
-//echo $json;
+//$parr=json_decode($json);
+/*
+//从数组中取10个随机下标
 $randArr=array_rand($parr,10);
 $arr=array();
 for($i=0;$i<count($randArr);$i++){
@@ -11,4 +13,5 @@ for($i=0;$i<count($randArr);$i++){
     array_push($arr,$parr[$randIndex]);
 }
 echo json_encode($arr);
+*/
 ?>
